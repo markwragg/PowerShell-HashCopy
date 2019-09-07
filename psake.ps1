3de4ce93b211ced0642be8c640e8681a5a6542b9
Mark Wragg <mark.wragg@elysianit.com>
2019-09-07 08:59:01 +0100

    Fix path to readme for non-Windows VM

--- psake.ps1
+++ psake.ps1
@@ -64,7 +64,7 @@ Task Build -Depends Test {
 
     "`n`tSTATUS: Running Set-ShieldsIoBadge to update Readme.md with $CoveragePercent% code coverage badge"
 
-    Set-ShieldsIoBadge -Subject 'Coverage' -Status $CoveragePercent -AsPercentage
+    Set-ShieldsIoBadge -Path (Join-Path $ProjectRoot '/README.md') -Subject 'Coverage' -Status $CoveragePercent -AsPercentage
 
     "`n"
 }
